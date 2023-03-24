ef165a44b6490bdee15e6f87381cd46bc7f23c7c
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-24 20:59:12 +0500

    Variable

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-#Complex Datatype
-complex 
-#Decimal to Binary
-print(bin(2131))
-#Binary to decimal
-print(int('0b100001010011', 2))
-
-#Decimal to hexaDecimal
-print(hex(1231));
-
-#HexaDecimal to Decimal
-print(int('0x4cf', 16))
-
-#Decimal to Octal
-print(oct(1231))
-#Octal to decimal
-print(int('0o2347', 8))
+#Variable Example
+user_name = input("What is your name? ")
+print("Hello " + user_name + "!")
+#Constant Variable Example
+PI = 3.14
+print(PI)
