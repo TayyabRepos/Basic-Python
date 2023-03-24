95934efd1d86069ac807ecc9ee5f61a715747d7c
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-25 00:38:35 +0500

    Strings indexers

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-#Formatted String
-name="Tayyab"
-age=22
-print(f"Hello {name} your age is {age}")
+#Strings indexers
+#Strings are immutable
+number="0123456789"
+#[start:stop:stepover]
+print(number[0:10:1])
+print(number[4:9:2])
+print(number[0:4:1])
