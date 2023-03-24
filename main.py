c6e145ed5e458ff70de47094ef7de5976874e534
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-25 00:16:23 +0500

    LongString

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-#Augmented assignment operator
-value = 10
-value += 5
-print(value)
+#Long Sting
+LongString = '''
+This is a long string
+and it continues here
+and it's not over yet
+'''
+print(LongString)
