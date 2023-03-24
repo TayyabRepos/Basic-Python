054d37e02601606f1dfb236f54be4c4e9d9049ee
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-25 00:44:37 +0500

    String immutability check

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-#Strings indexers
-#Strings are immutable
-number="0123456789"
-#[start:stop:stepover]
-print(number[0:10:1])
-print(number[4:9:2])
-print(number[0:4:1])
-print(number[::-1])
-print(number[::-2])
-print(number[-1])
-print(number[-2])
-print(number[-3])
-print(number[-4])
-print(number[-5])
-print(number[-6])
-print(number[-7])
-print(number[-8])
-print(number[-9])
-print(number[-10])
+#String immutability
+name= "John"
+name[1] = "a" #error because strings are immutable we can't change the value of a string
+print(name)
