1931b0a78142cb499ed3572e06a401e210905dde
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-24 20:39:50 +0500

    Operator Precedence

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-# Math functions
-from cmath import sqrt
-from math import factorial, log
+#Operator Precedence
 
+# Order of precedence
+# ()
+# **
+# */
+# +-
 
-
-print(round(7.7))
-print(abs(-7.7))
-print(factorial(5))
-print(sqrt(25))
-print(pow(2, 3))
-print(max(2, 3))
-print(log(2, 3))
+print((220*2)+2*8**2)
