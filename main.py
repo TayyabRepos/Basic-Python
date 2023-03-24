99d304a4c863ea8a900974626d6dacf828304ddd
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-25 03:12:15 +0500

    List slicing

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-#List
+# List slicing
+# lists are mutable, so we can change the values of the list
 amazon_cart = ['notebooks', 'sunglasses', 'toys', 'grapes']
-for item in amazon_cart:
-    print(item.capitalize())
+amazon_cart[0] = 'laptop'
+# print(amazon_cart[0::2])  # amazon_cart create a new list
+# print(amazon_cart)
+
+new_cart = amazon_cart  # Here we are not creating a new list instead we are just referencing the list
+new_cart[0] = 'gum'
+print(new_cart)
+print(amazon_cart)
+
+new_cart2 = amazon_cart[:]  # copy the list here we are creating a new list
+print(new_cart2)
