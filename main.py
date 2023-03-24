8fef7ff92b92561c6be9994447d5a79e7f36c190
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-24 21:22:44 +0500

    Augmented assignment operator

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# Expression vs Statement
-#Expression: An expression is a combination of values, variables, operators, and calls to functions.
-# Statement: A statement is an instruction that the Python interpreter can execute.
-
-value = 15; # This is a statement
-
-result = value + 10; # This is a statement (result = value + 10) and this expression (value + 10) 
+#Augmented assignment operator
+value = 10
+value += 5
+print(value)
