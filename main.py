95260bd4e7accef76b9e31e45c925d229e8d38ce
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-25 03:00:37 +0500

    List

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-#Password Username
-Username = input("What is your name? ")
-Password = input("What is your password? ")
-
-x= len(Password)
-
-print(f'Hello {Username}! Your password is {"*"*x} and {x} letters long')
+#List
+amazon_cart = ['notebooks', 'sunglasses', 'toys', 'grapes']
+for item in amazon_cart:
+    print(item.capitalize())
