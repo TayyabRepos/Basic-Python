a40d33a89a17e3d4d349f200e83f504b0c9107ba
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-24 21:04:07 +0500

    Statement and expression

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#Variable Example
-user_name = input("What is your name? ")
-print("Hello " + user_name + "!")
-#Constant Variable Example
-PI = 3.14
-print(PI)
+# Expression vs Statement
+#Expression: An expression is a combination of values, variables, operators, and calls to functions.
+# Statement: A statement is an instruction that the Python interpreter can execute.
+
+value = 15; # This is a statement
+
+result = value + 10; # This is a statement (result = value + 10) and this expression (value + 10) 
