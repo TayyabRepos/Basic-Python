a989b0b73c6171e90fc7303b505d3492d45d1690
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-25 16:50:41 +0500

    List sort and reverse()

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ basket = [75,1,5,2,6,12,14,33,22,4,6]
 # print(sorted(basket)) #Creates new list
 # print(basket)
 
-new_list=basket[:]
-sorted(new_list)
+new_list=basket.copy()
 new_list.sort()
+new_list.reverse()
 print(new_list)
