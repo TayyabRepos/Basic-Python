3231d1900d2d72fc3ba4e02df78c9b9e7f66aca9
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-26 00:14:55 +0500

    Conditions

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-#Sets methods
-#Discard()
-#.Difference()
-#.Difference_update()
-#.intersection()
-#.isdisjoint()
-#.issubset()
-#.issuperset()
-#.union()
+# Coditions
+is_old= True
+is_licensed=True
 
-my_set = {4,5}
-your_set = {4,5,6,7,8,9,10}
-# my_set.discard(5)
-# print(my_set.difference(your_set))
-# print(my_set)
-# my_set.difference_update(your_set)
-# print(my_set)
-# print(my_set.intersection(your_set))
-# print(my_set.isdisjoint(your_set))
-# print(my_set | your_set)
-# print(my_set & your_set)
-# print(my_set.issubset(your_set))
-# print(my_set.issuperset(your_set))
-# print(your_set.issuperset(my_set))
+if is_old and is_licensed:
+    print('You are old enough to drive a car')
+else:
+    print('You are not old enough to drive a car')
