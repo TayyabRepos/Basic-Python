6619c1a45ddb1ca7138405f3775f8532624fb613
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-25 17:10:15 +0500

    .join()

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 # List Methods
-basket = [75,1,5,2,6,12,14,33,22,4,6]
-# basket.sort() #Modify basket
-# print(sorted(basket)) #Creates new list
-# print(basket)
+sentence = " ".join(['Hi', 'My','Name', 'Is', 'Tayyab'])
 
-new_list=basket.copy()
-new_list.sort()
-new_list.reverse()
-print(new_list)
+print(sentence)
