471fb272523222d96d27a300dbe8f63cd7aa7d1d
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-25 18:00:35 +0500

    Dictionary built-in methods

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 #Key in dictionary should be immutable
 #Key in dictionary should be unique otherwise it will overwrite
 
+#in
+#keys
+#values
+#item
+#clear
+#copy
+#pop('age')
+#popitem 'Randomly pop key values item' Useful to destructively iterate over a dictionary
+#update({age: 100})
 dictionary={
     '12':[1,2],
     True: 'Hello',
     'is_Magic':False,
-    # 'age': 100
+     'age': 100
 }
 
-print(dictionary.get('age',12))
+print(dictionary.keys())
 
 
 # print(dictionary['12'])
 # print(dictionary[True])
 # print(dictionary['is_Magic'])
+
