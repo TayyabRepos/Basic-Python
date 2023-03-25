deb4703ed39e4d77ed9e57fadfadf90ab4f1d603
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-25 17:16:54 +0500

    List Unpacking

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# List Methods
-sentence = " ".join(['Hi', 'My','Name', 'Is', 'Tayyab'])
-
-print(sentence)
+# List Unpacking
+a, b, c, d,e, *others,f  = [1, 2, 3, 4, 5,6,7,8,9,10]
+print(a)
+print(others)
+print(f)
