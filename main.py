4fde6612f5fafad92b59c565d4cd7722f5ab1caa
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-25 16:16:35 +0500

    List Methods

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# List slicing
-# lists are mutable, so we can change the values of the list
-amazon_cart = ['notebooks', 'sunglasses', 'toys', 'grapes']
-amazon_cart[0] = 'laptop'
-# print(amazon_cart[0::2])  # amazon_cart create a new list
-# print(amazon_cart)
+#List Methods
+basket=[1,2,3,4,5]
 
-new_cart = amazon_cart  # Here we are not creating a new list instead we are just referencing the list
-new_cart[0] = 'gum'
-print(new_cart)
-print(amazon_cart)
 
-new_cart2 = amazon_cart[:]  # copy the list here we are creating a new list
-print(new_cart2)
+basket.append(12)
+basket.extend([100,101])
+basket.insert(8, 102)
+print(basket);
+basket.remove(102)
+print(basket)
+basket.pop(5)
+print(basket)
+basket.reverse()
+print(basket)
