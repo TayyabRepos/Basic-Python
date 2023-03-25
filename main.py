a65b26b8a1967e87410a6e0a0185c22bde7548e6
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-25 17:35:15 +0500

    Dictionary

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-# List Unpacking
-a, b, c, d,e, *others,f  = [1, 2, 3, 4, 5,6,7,8,9,10]
-print(a)
-print(others)
-print(f)
+# Dictionary is unorderd key paird value
+
+dictionary = {
+    'a': [1, 2, 3],
+    'b': 'Hello',
+    'c': True
+}
+
+new_list = [
+    {
+        'a': [1, 2, 3],
+        'b': 'Hello',
+        'c': True
+    },
+    {
+        'a': [4, 5, 6],
+        'b': 'Hello',
+        'c': True
+    },
+    {
+        'a': [7, 6, 9],
+        'b': 'Hello',
+        'c': True
+    }
+]
+
+list = new_list[0]['a'][2]
+print(list)
