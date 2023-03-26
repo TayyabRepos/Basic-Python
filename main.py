744b63ee1ad7159c336ff09ee8d3f416cc47e94e
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-26 16:24:43 +0500

    RangeFirst GUI in python

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-#Range
-counter = []
-for _ in range(1,11):
-    new_list = list (range(1,11))
-    counter = counter + new_list
-print(counter)
+#First GUI in python
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+]
+
+for image in picture:
+    for pixel in image:
+        if(pixel==1):
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print('')
+
