84debbe0b1ede06a8defb5495326d039e0bc05e4
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-26 05:43:34 +0500

    Range

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-#for loop
-
-# for item in [1,2,3,4,5,6,7,8,9,10]:
-#     for x in ['a','b','c']:
-#         print(item,x)
-
-user = {
-    'name': 'Golem',
-    'age': 5006,
-    'can_swim': False
-}
-for item in user:
-    print(item)
-
-print('_________')
-for item, value in user.items():
-    print(item, value)
-print('_________')
-for item in user.values():
-    print(item)
-print('_________')
-for item in user.keys():
-    print(item)
+#Range
+counter = []
+for _ in range(1,11):
+    new_list = list (range(1,11))
+    counter = counter + new_list
+print(counter)
