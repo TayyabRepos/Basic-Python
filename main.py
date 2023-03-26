47ba47c206dfe0717ed8106a89a6c3971a3429bc
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-26 16:56:59 +0500

    Duplicates

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-#First GUI in python
-picture = [
-    [0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 1, 1, 1, 0, 0],
-    [0, 1, 1, 1, 1, 1, 0],
-    [1, 1, 1, 1, 1, 1, 1],
-    [0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0],
-]
+#Duplicates
 
-for image in picture:
-    for pixel in image:
-        if(pixel==1):
-            print('*', end='')
-        else:
-            print(' ', end='')
-    print('')
+some_list= ['a','b','c','b','d','m','n','n']
+
+duplicates = []
+
+for value in some_list:
+    if some_list.count(value)>1:
+        if value not in duplicates:
+            duplicates.append(value)
+print(duplicates)
+# result = set(duplicates)
+# some_list = list(result.union())
+# print(some_list)
 
