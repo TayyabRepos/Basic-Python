280df3a72b85e0f217d191e4067769830ffe8d59
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-26 05:20:14 +0500

    for loops

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-#is operator
-print(1 is 1)
-print(1 is not 1)
-print(1 is 2)
-print(1 is not 2)
-print(1 is 1.0)
-print(1 is not 1.0)
-print(1 is 1.0)
+#for loop
+
+# for item in [1,2,3,4,5,6,7,8,9,10]:
+#     for x in ['a','b','c']:
+#         print(item,x)
+
+user = {
+    'name': 'Golem',
+    'age': 5006,
+    'can_swim': False
+}
+for item in user:
+    print(item)
+
+print('_________')
+for item, value in user.items():
+    print(item, value)
+print('_________')
+for item in user.values():
+    print(item)
+print('_________')
+for item in user.keys():
+    print(item)
