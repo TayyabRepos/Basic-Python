b6cb0f99fb75daf5854bc84f5d201f36963fd344
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-28 02:50:39 +0500

    comprehension exercise

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
-#set and dictionary comprehention
+#comprehension exercise
+some_list = [1,1,2,3,4,4,5,5,3,1,2,3,4,1,6,6]
 
-# set1 = {char for char in 'hello'}
-# set2 = {num for num in range(1,101)}
-# set3 = {num**2 for num in range(1,101)}
-# set4 = {num for num in range(1,101) if num%2 ==0}
-#
-# print(set1)
-# print(set2)
-# print(set3)
-# print(set4)
+duplicates = set([dubplicate for dubplicate in some_list if some_list.count(dubplicate)>1  ])
 
-
-# items1={
-#     'a': 1,
-#     'b': 2
-# }
-#
-# my_dict = {key:value**2   for key,value in items1.items()}
-
-my_dict = {num:num+1 for num in [1,2,3,4,5,6,7,8,9]}
-
-print(my_dict)
-
-
-
+print(duplicates)
