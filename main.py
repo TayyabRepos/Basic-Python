c6caa50db628c93d50c382a2b9b2287d90f6ff9e
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-27 10:25:48 +0500

    Zip

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-#Map, filter
+#Zip
 
 my_list=[1,2,3,4,5,6,7,8]
+your_list=[9,10,11,12,13,14,15,16]
 
-def multiplyBy2(item):
-    return item*2
-
-def only_odd(item):
-    return item%2 != 0
-
-print(list(map(multiplyBy2, my_list)))
-
-print(list(filter(only_odd, my_list)))
+print(list(zip(my_list, your_list)))
