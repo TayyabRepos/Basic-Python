c0697484cbe48a9f5165c5fb0d1650a6b7a9f89f
Tayyab <tayyab.shahzad100000@gmail.com>
2023-03-28 02:42:12 +0500

    set and dictionary comprehension

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-#List comprehention
+#set and dictionary comprehention
 
-my_list1 = [char for char in 'hello']
-my_list2 = [num for num in range(1,101)]
-my_list3 = [num**2 for num in range(1,101)]
-my_list4 = [num for num in range(1,101) if num%2 ==0]
+# set1 = {char for char in 'hello'}
+# set2 = {num for num in range(1,101)}
+# set3 = {num**2 for num in range(1,101)}
+# set4 = {num for num in range(1,101) if num%2 ==0}
+#
+# print(set1)
+# print(set2)
+# print(set3)
+# print(set4)
 
-print(my_list1)
-print(my_list2)
-print(my_list3)
-print(my_list4)
+
+# items1={
+#     'a': 1,
+#     'b': 2
+# }
+#
+# my_dict = {key:value**2   for key,value in items1.items()}
+
+my_dict = {num:num+1 for num in [1,2,3,4,5,6,7,8,9]}
+
+print(my_dict)
+
+
+
